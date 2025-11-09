5184c0b3f85c859a2e0b1aa4814db874aca60602
Ishaan-C <ishaanchakraborty2009@gmail.com>
2025-11-09 10:10:23 +0530

    Added explanatory comments for Two Sum solution

--- TwoSum.java
+++ TwoSum.java
@@ -4,17 +4,17 @@ class Solution {
         boolean fl=false;
         outer: for(int i=0;i<nums.length;i++){
            inner: for(int j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
+            if(nums[i]+nums[j]==target){ //checking if the sum of an element with consequent elements equals target
                 m=i;
                 n=j;
                 fl=true;
-                break outer;
+                break outer;  //breaking both loops as soon as we find the first such pair
             }
            }
 
         }
         if(fl){
-            return new int[]{m,n};
+            return new int[]{m,n};   
         }
         else{
             return null;
