66e22c85bfdbd28c1f0b987741d97aed1b31413d
Ishaan C <ishaanchakraborty2009@gmail.com>
2025-11-12 17:05:11 +0530

    Update solution for Longest Common Prefix

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,31 +1,30 @@
-public class LongestCommonPrefix{
+public class LongestCommonPrefix {
 
     public String longestCommonPrefix(String[] strs) {
 
         char c;
+        String res = "";
 
-        String res="";
+        outer: for (int i = 0; i < strs[0].length(); i++) {
 
-        outer: for(int i=0;i<strs[0].length();i++){
+            c = strs[0].charAt(i);
+            res += c;
 
-            c=strs[0].charAt(i);
+            inner: for (int j = 0; j < strs.length; j++) {
 
-            res+=c;
-
-            inner: for(int j=0;j<strs.length;j++){
-
-                 if(strs[j].startsWith(res)){
-                continue inner;
+                if (strs[j].startsWith(res)) {
+                    continue inner;
+                } else {
+                    // Mismatch found, so remove the last added character
+                    res = res.substring(0, res.length() - 1);
+                    break outer;
                 }
-
-                else{
-                break outer;}
             }
+        }
 
-            
+        // If we never broke early (res == strs[0]), just return res as is
+        return res;
     }
-    return res.substring(0,res.length()-1);
 }
-    }
 
 
