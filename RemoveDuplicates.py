a66af358327acf275b8d3986cdba965cdf362315
Ishaan C <ishaanchakraborty2009@gmail.com>
2025-11-15 07:57:53 +0530

    Added CORRECT solution for Remove Duplicates from a Sorted Array question

--- RemoveDuplicates.py
+++ RemoveDuplicates.py
@@ -1,35 +1,19 @@
-'''OLD ALGORITHM-
-1) IDENTIFY DUPLICATES IN THE LIST.
-2) DETECT UNIQUE ELEMENTS IN THE LIST USING A DIFFERENT FUNCTION.
-3) RETURN THE INDEX OF THE UNIQUE ELEMENT.
-4) SWAP A DUPLICATE WITH A UNIQUE ELEMENT.
-5) RETURN THE MODIFIED LIST.
-PROBLEMS: AFTER SWAPPING IS DONE, LATER COMPARISIONS ARE MADE WITH THE SWAPPED ELEMENTS, WHICH MAY LEAD TO INCORRECT RESULTS. WE ONLY NEED TO COMPARE THE ORIGINAL ELEMENTS.'''
+#CORRECT APPROACH
 
 def remove_duplicates(input_list):
-    count=0
-    i = 1  # Start from the second element because first element is always unique
 
-    while i < len(input_list):
-        
-        if input_list[i]==input_list[i-1]:
-            next_index = find_unique(input_list, i)
-            if next_index is not None:
-                input_list[i], input_list[next_index] = input_list[next_index], input_list[i]
-                count+=1
-            else:
-                break
-        i += 1
+    if not input_list:                        #if list is empty
+        return 0
 
+    i=1                                      #pointer for next unique element position. Element at 0 is always unique
+    for j in range(1,len(input_list)):         
+        if input_list[j] != input_list[j-1]:        #i.e if current element is unique
+            input_list[i] = input_list[j]           #move unique element to the front
+            i+=1
 
-    return len(input_list)-count
+    return i                                   #return count of unique elements 
 
-def find_unique(input_list,index):
-    for j in range(index+1,len(input_list)):
-        if input_list[j]!=input_list[index]:
-            return j
-        
-    return None
+    
 
 
     
